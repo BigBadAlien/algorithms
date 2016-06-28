bca5af8e9ef29c35bc1e94ff639735986688a62d
Alexandr Ishchenko <ishchenko@wamba.com>
2016-06-28 20:40:35 +0300

    Added count of loops for partial fractal calculation

--- src/math.js
+++ src/math.js
@@ -1,20 +1,47 @@
-var NegativeValueImpossible = new Error('Negative value impossible');
-module.exports.NegativeValueImpossible = NegativeValueImpossible;
+var isInteger = require('../src/types').isInteger;
+
+var NegativeValueImpossibleError = new Error('Negative value impossible');
+module.exports.NegativeValueImpossible = NegativeValueImpossibleError;
+
+var OnlyIntegerAvailableError = new Error('Only integer available');
+module.exports.OnlyIntegerAvailableError = OnlyIntegerAvailableError;
+
+var СontradictoryParamsError = new Error('Сontradictory params');
+module.exports.СontradictoryParamsError = СontradictoryParamsError;
 
 /**
  * Get factorial of number
  * @param {!number} value
+ * @param {=number} loops
  * @return {number}
  */
-function getFactorial(value) {
+function getFactorial(value, loops) {
+  if (!isInteger(value)) {
+    throw OnlyIntegerAvailableError;
+  }
+
   if (value < 0) {
-    throw NegativeValueImpossible;
+    throw NegativeValueImpossibleError;
   }
-  else if (value == 0) {
+
+  if (isInteger(loops) && loops < 0) {
+    throw NegativeValueImpossibleError;
+  }
+
+  if (isInteger(loops) && loops > value) {
+    throw СontradictoryParamsError;
+  }
+
+  if (value == 0) {
     return 1;
-  }
-  else {
-    return (value * getFactorial(value - 1));
+  } else {
+    if (isInteger(loops) && loops > 1) {
+      return (value * getFactorial(value - 1, loops - 1));
+    } else if (isInteger(loops) && loops === 1) {
+      return value;
+    } else {
+      return (value * getFactorial(value - 1));
+    }
   }
 }
 module.exports.getFactorial = getFactorial;
--- test/math.js
+++ test/math.js
@@ -1,18 +1,46 @@
 var expect = require('chai').expect;
 var getFactorial = require('../src/math').getFactorial;
 var NegativeValueImpossible = require('../src/math').NegativeValueImpossible;
+var OnlyIntegerAvailableError = require('../src/math').OnlyIntegerAvailableError;
+var СontradictoryParamsError = require('../src/math').СontradictoryParamsError;
 
 describe("Math", function () {
   describe("Factorial", function () {
     it("Factorial of 5 is 120", function () {
       expect(getFactorial(5)).to.equal(120);
+    });
+
+    it("Factorial of 44 is 2.6582715747884485e+54", function () {
+      expect(getFactorial(5)).to.equal(120);
       expect(getFactorial(44)).to.equal(2.6582715747884485e+54);
+    });
+
+    it("Result with value 36 with 6 loops is 36*35*34*33*32*31", function () {
+      expect(getFactorial(36, 6)).to.equal(36 * 35 * 34 * 33 * 32 * 31);
+    });
+    //
+    it("Result with value 79 with 4 loops is 1402410240", function () {
+      expect(getFactorial(79, 4)).to.equal(79 * 78 * 77 * 76);
     });
 
     it("If param is negative number expected throwing", function () {
       expect(getFactorial.bind(this, -787)).to.throw(NegativeValueImpossible);
       expect(getFactorial.bind(this, -1024)).to.throw(NegativeValueImpossible);
-      expect(getFactorial.bind(this, -67567567.78567867)).to.throw(NegativeValueImpossible);
+    });
+
+    it("If loops is less than zero integer expected throwing", function () {
+      expect(getFactorial.bind(this, 32, -7)).to.throw(NegativeValueImpossible);
+    });
+
+    it("If loops more than value expected throwing", function () {
+      expect(getFactorial.bind(this, 32, 36)).to.throw(СontradictoryParamsError);
+    });
+
+    it("If value is not integer expected throwing", function () {
+      expect(getFactorial.bind(this, '32', 36)).to.throw(OnlyIntegerAvailableError);
+      expect(getFactorial.bind(this, 'foobar', 36)).to.throw(OnlyIntegerAvailableError);
+      expect(getFactorial.bind(this, true, 36)).to.throw(OnlyIntegerAvailableError);
+      expect(getFactorial.bind(this, Infinity, 36)).to.throw(OnlyIntegerAvailableError);
     });
   });
 });
