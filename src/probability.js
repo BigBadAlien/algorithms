c6c651b69489bf1ab9e0c4c024db61c2c8401584
Alexandr Ishchenko <ishchenko@wamba.com>
2016-06-29 16:01:15 +0300

    Added getProbability implementation

--- src/probability.js
+++ src/probability.js
@@ -18,9 +18,44 @@ module.exports.OnlyIntegerAvailableError = OnlyIntegerAvailableError;
  * @return {number}
  */
 function getProbability(sampleSpace, choice, sample) {
+  if (!isInteger(sampleSpace) || !isInteger(choice)) {
+    throw OnlyIntegerAvailableError;
+  }
 
+  if (sampleSpace < 0 ||
+    choice < 0 ||
+    (isInteger(sample) && sample < 0)) {
+    throw NegativeParamError;
+  }
+
+  if (choice > sampleSpace) {
+    throw СontradictoryParamsError;
+  }
+
+  if (isInteger(sample) && (sample < choice || sample > sampleSpace)) {
+    throw СontradictoryParamsError;
+  }
+
+  if (sampleSpace === 0 ||
+    choice === 0 ||
+    sample === 0) {
+    return 0;
+  }
+
+  if (!isInteger(sample)) {
+    return 1 / getCombinationsCount(sampleSpace, choice);
+  }
+
+  if ((isInteger(sample) && choice === 1)) {
+    return 1 / getCombinationsCount(sampleSpace, sample);
+  }
+
+  return (getCombinationsCount(sample, choice) *
+    getCombinationsCount(sampleSpace - choice, sample - choice)) /
+    getCombinationsCount(sampleSpace, sample);
 }
 module.exports.getProbability = getProbability;
+console.log(getProbability(77, 5));
 
 /**
  * Get combination count by sample space and sample size.
--- test/probability.js
+++ test/probability.js
@@ -58,26 +58,21 @@ describe("Probability", function () {
     });
 
     it("if you choose 5 values of the 77, expected probability of assigning is 5.0613*10^-8", function () {
-      expect(getProbability(77, 5)).to.equal(5.0613 * Math.pow(10, -8));
+      expect(getProbability(77, 5)).to.equal(1 / ((77 * 76 * 75 * 74 * 73) / (5 * 4 * 3 * 2)));
     });
 
     it("if you choose 1 values of the 77 with sample length 5, expected probability of assigning is 5.2019*10^-8", function () {
-      expect(getProbability(77, 5, 1)).to.equal(5.2019 * Math.pow(10, -8));
+      expect(getProbability(77, 1, 5)).to.equal(1 / ((77 * 76 * 75 * 74 * 73) / (5 * 4 * 3 * 2)));
     });
 
     it("if you choose 2 values of the 36 with sample length 6, expected probability of assigning is 0.2661", function () {
-      expect(getProbability(36, 6, 2)).to.equal(0.2661);
+      expect(getProbability(36, 2, 6)).to.equal((((6 * 5) / 2) * ((34 * 33 * 32 * 31) / (4 * 3 * 2))) / ((36 * 35 * 34 * 33 * 32 * 31) / (6 * 5 * 4 * 3 * 2)));
     });
 
     it("if one of the params is zero expected results is zero too", function () {
-      expect(getProbability(6567, 787, 0)).to.equal(0);
-      expect(getProbability(8798, 0, 0)).to.equal(0);
       expect(getProbability(8798, 0)).to.equal(0);
+      expect(getProbability(8798, 0, 788)).to.equal(0);
       expect(getProbability(0, 0, 0)).to.equal(0);
-    });
-
-    it("if sample more than sample space expected throwing", function () {
-      expect(getProbability(6768, 798798, 7668)).to.throw(СontradictoryParamsError);
     });
 
     it("if sample space, sample or choice less than zero expected throwing", function () {
@@ -88,9 +83,38 @@ describe("Probability", function () {
       expect(getProbability.bind(this, -4534, -16)).to.throw(NegativeParamError);
     });
 
-    it("if choice more than sample expected throwing", function () {
-      expect(getProbability.bind(this, 8976, 566, 999)).to.throw(СontradictoryParamsError);
+    it("if choice more than sample space expected throwing", function () {
       expect(getProbability.bind(this, 6768, 9887)).to.throw(СontradictoryParamsError);
+      expect(getProbability.bind(this, 6768, 798798, 7668)).to.throw(СontradictoryParamsError);
+    });
+
+    it("if sample passed and it less than choice or more than sample space expected throwing", function () {
+      expect(getProbability.bind(this, 12767, 9887, 9886)).to.throw(СontradictoryParamsError);
+      expect(getProbability.bind(this, 5456, 9887, 9889)).to.throw(СontradictoryParamsError);
+    });
+
+    it("if sample not integer type expected its ignoring", function () {
+      expect(getProbability(100, 1, '342')).to.equal(0.01);
+      expect(getProbability(100, 2, null)).to.equal(1 / 4950);
+      expect(getProbability(77, 5, true)).to.equal(1 / ((77 * 76 * 75 * 74 * 73) / (5 * 4 * 3 * 2)));
+      expect(getProbability(77, 5, Infinity)).to.equal(1 / ((77 * 76 * 75 * 74 * 73) / (5 * 4 * 3 * 2)));
+    });
+
+    it("if sample space or choice not integer expected throwing", function () {
+      expect(getProbability.bind(this, '36', 6)).to.throw(OnlyIntegerAvailableError);
+      expect(getProbability.bind(this, 44, '6')).to.throw(OnlyIntegerAvailableError);
+      expect(getProbability.bind(this, 36, 6.7)).to.throw(OnlyIntegerAvailableError);
+      expect(getProbability.bind(this, 36.565, 6)).to.throw(OnlyIntegerAvailableError);
+      expect(getProbability.bind(this, 345.565e+445, 6)).to.throw(OnlyIntegerAvailableError);
+      expect(getProbability.bind(this, 345, 6e+786786)).to.throw(OnlyIntegerAvailableError);
+      expect(getProbability.bind(this, 44, '6')).to.throw(OnlyIntegerAvailableError);
+      expect(getProbability.bind(this, 78, Infinity)).to.throw(OnlyIntegerAvailableError);
+      expect(getProbability.bind(this, Infinity, 71)).to.throw(OnlyIntegerAvailableError);
+      expect(getProbability.bind(this, 78, -Infinity)).to.throw(OnlyIntegerAvailableError);
+      expect(getProbability.bind(this, -Infinity, 71)).to.throw(OnlyIntegerAvailableError);
+      expect(getProbability.bind(this)).to.throw(OnlyIntegerAvailableError);
+      expect(getProbability.bind(this, null, 71)).to.throw(OnlyIntegerAvailableError);
+      expect(getProbability.bind(this, 24, null)).to.throw(OnlyIntegerAvailableError);
     });
   });
 });
